b015bd3bf11caaf9080fcb06f19527d2fe95fbe2
Will <will-soto@pluralsight.com>
2019-04-30 16:27:34 -0400

    partial module 2
    Signed-off-by: Will <will-soto@pluralsight.com>

--- src/app.js
+++ src/app.js
@@ -9,8 +9,19 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), {
+  encoding: 'utf8'
+});
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), {
+  encoding: 'utf8'
+});
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => res.render('index', {
-  title: 'index'
+  title: 'Account Summary',
+  accounts
 }));
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
