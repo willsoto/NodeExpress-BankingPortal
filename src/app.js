5fe07ce865eb8c51e1a2ea83e4ac39ae2bed3926
Will <will-soto@pluralsight.com>
2019-05-01 08:52:14 -0400

    module 2
    Signed-off-by: Will <will-soto@pluralsight.com>

--- src/app.js
+++ src/app.js
@@ -24,4 +24,20 @@ app.get('/', (req, res) => res.render('index', {
   accounts
 }));
 
+app.get('/savings', (req, res)=> res.render('account', {
+  account: accounts.savings
+}));
+
+app.get('/checking', (req, res)=> res.render('account', {
+  account: accounts.checking
+}));
+
+app.get('/credit', (req, res)=> res.render('account', {
+  account: accounts.credit
+}));
+
+app.get('/profile', (req, res)=> res.render('profile', {
+  user: users[0]
+}));
+
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
